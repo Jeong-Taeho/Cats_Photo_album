b33280fd9a97ad38d7e70e8cb61884ecdfe75366
Jeong-Taeho <117630589+Jeong-Taeho@users.noreply.github.com>
2023-07-13 23:50:34 +0900

    feat : 최상위 상태 관리

--- src/App.js
+++ src/App.js
@@ -1,125 +1,126 @@
-import Nodes from "./Nodes.js";
-import { request } from "./api.js";
-import ImageViewer from "./ImageViewer.js";
-import Loading from "./Loading.js";
-import Breadcrumb from "./Breadcrumb.js";
+import Nodes from './components/Nodes.js';
+import { request } from './api/api.js';
+import ImageViewer from './components/ImageViewer.js';
+import Loading from './components/Loading.js';
+import Breadcrumb from './components/Breadcrumb.js';
+import { API_END_POINT } from './api/api.js';
 
 export default function App({ $target }) {
-	this.state = {
-		isRoot: true,
-		isLoading: false,
-		nodes: [],
-		paths: [],
-	};
+  this.state = {
+    isRoot: true,
+    isLoading: false,
+    nodes: [],
+    paths: [],
+    selectedImageUrl: '',
+  };
 
-	const breadcrumb = new Breadcrumb({
-		$target,
-		initialState: this.state.paths,
-		onBreadcrumbClick: async id => {
-			//선택한 경로 외에 남은 paths를 날려주는 처리를 해야한다.
-			if (id) {
-				const nextPaths = [...this.state.paths];
-				const movePathIndex = nextPaths.findIndex(path => id === path.id);
-				this.setState({
-					...this.state,
-					paths: nextPaths.slice(0, movePathIndex + 1),
-				});
-			} else {
-				this.setState({
-					...this.state,
-					paths: [],
-				});
-			}
+  const loading = new Loading({
+    $target,
+  });
 
-			await fetchNodes(id);
-		},
-	});
+  const breadcrumb = new Breadcrumb({
+    $target,
+    initialState: this.state.paths,
+    onBreadcrumbClick: async (id) => {
+      //선택한 경로 외에 남은 paths를 날려주는 처리를 해야한다.
+      if (id) {
+        const nextPaths = [...this.state.paths];
+        const movePathIndex = nextPaths.findIndex((path) => id === path.id);
+        this.setState({
+          ...this.state,
+          paths: nextPaths.slice(0, movePathIndex + 1),
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          paths: [],
+        });
+      }
 
-	const nodes = new Nodes({
-		$target,
-		initialState: {
-			isRoot: this.state.isRoot,
-			nodes: this.state.nodes,
-			selectedImageUrl: null,
-		},
-		onClick: async node => {
-			if (node.type === "DIRECTORY") {
-				fetchNodes(node.id);
-				this.setState({
-					...this.state,
-					paths: [...this.state.paths, node],
-				});
-			}
+      await fetchNodes(id);
+    },
+  });
 
-			if (node.type === "FILE") {
-				this.setState({
-					...this.state,
-					selectedImageUrl: `https://kdt-frontend.cat-api.programmers.co.kr/static${node.filePath}`,
-				});
-			}
-		},
-		onPrevClick: async () => {
-			const nextPaths = this.state.paths;
-			nextPaths.pop();
-			this.setState({
-				...this.state,
-				paths: nextPaths,
-			});
+  const nodes = new Nodes({
+    $target,
+    initialState: {
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    },
+    onClick: async (node) => {
+      if (node.type === 'DIRECTORY') {
+        fetchNodes(node.id);
+        this.setState({
+          ...this.state,
+          paths: [...this.state.paths, node],
+        });
+      }
 
-			if (nextPaths.length === 0) {
-				await fetchNodes();
-			} else {
-				await fetchNodes(nextPaths[nextPaths.length - 1].id);
-			}
-		},
-	});
+      if (node.type === 'FILE') {
+        this.setState({
+          ...this.state,
+          selectedImageUrl: `${API_END_POINT}/static${node.filePath}`,
+        });
+      }
+    },
+    onPrevClick: async () => {
+      const nextPaths = [...this.state.paths];
+      nextPaths.pop();
+      this.setState({
+        ...this.state,
+        paths: nextPaths,
+      });
 
-	const imageViewer = new ImageViewer({
-		$target,
-		onImageClose: () => {
-			this.setState({
-				...this.state,
-				selectedImageUrl: null,
-			});
-		},
-	});
+      if (nextPaths.length === 0) {
+        await fetchNodes();
+      } else {
+        await fetchNodes(nextPaths[nextPaths.length - 1].id);
+      }
+    },
+  });
 
-	const loading = new Loading({
-		$target,
-	});
+  const imageViewer = new ImageViewer({
+    $target,
+    onImageClose: () => {
+      this.setState({
+        ...this.state,
+        selectedImageUrl: '',
+      });
+    },
+  });
 
-	this.setState = nextState => {
-		this.state = nextState;
+  this.setState = (nextState) => {
+    this.state = nextState;
 
-		breadcrumb.setState(this.state.paths);
+    nodes.setState({
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    });
 
-		nodes.setState({
-			isRoot: this.state.isRoot,
-			nodes: this.state.nodes,
-		});
+    imageViewer.setState({
+      selectedImageUrl: this.state.selectedImageUrl,
+    });
 
-		imageViewer.setState({
-			selectedImageUrl: this.state.selectedImageUrl,
-		});
+    loading.setState(this.state.isLoading);
 
-		loading.setState(this.state.isLoading);
-	};
+    breadcrumb.setState(this.state.paths);
+  };
 
-	const fetchNodes = async id => {
-		this.setState({
-			...this.state,
-			isLoading: true,
-		});
+  const fetchNodes = async (id) => {
+    this.setState({
+      ...this.state,
+      isLoading: true,
+    });
 
-		const nodes = await request(id ? `/${id}` : "/");
+    const nodes = await request(id ? `/${id}` : '/');
 
-		this.setState({
-			...this.state,
-			nodes,
-			isRoot: id ? false : true,
-			isLoading: false,
-		});
-	};
+    this.setState({
+      ...this.state,
+      nodes,
+      isRoot: id ? false : true,
+      isLoading: false,
+    });
+  };
 
-	fetchNodes();
+  fetchNodes();
 }
