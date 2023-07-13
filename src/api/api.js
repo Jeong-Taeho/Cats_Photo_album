d77d8ade46059985f3b9de553347ca52437851af
Jeong-Taeho <117630589+Jeong-Taeho@users.noreply.github.com>
2023-07-14 01:16:00 +0900

    feat : 환경 변수 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
-export const API_END_POINT = 'https://kdt-frontend.cat-api.programmers.co.kr';
+export const VITE_API_END_POINT = import.meta.env.VITE_API_END_POINT;
 
 export const request = async (url) => {
   try {
-    const res = await fetch(`${API_END_POINT}${url}`);
+    const res = await fetch(`${VITE_API_END_POINT}${url}`);
 
     if (!res.ok) {
       throw new Error('API 호출이 이상합니다');
